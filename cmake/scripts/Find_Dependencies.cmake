35058e2cd55ccfb3f00f83c0676ce632038a1c57
Marvin Smith <marvin_smith1@me.com>
2023-07-05 19:40:18 -0600

    Initial commit

---
+++ cmake/scripts/Find_Dependencies.cmake
@@ -0,0 +1,12 @@
+#    File:    Find_Packages.cmake
+#    Author:  Marvin Smith
+#    Date:    7/5/2023
+#
+
+#
+
+#-------------------------#
+#-      GoogleTest       -#
+#-------------------------#
+find_package( GTest REQUIRED )
+
---
+++ src/libwarpcore/image/Image.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file    Image.hpp
+ * @author  Marvin Smith
+ * @date    7/5/2023
+*/
+#pragma once
+
+namespace term::warp::io {
+
+
+
+} // End of WARP Namespace
