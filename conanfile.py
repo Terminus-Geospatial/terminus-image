5121f47c70fcaa6d9302efc5aee247a6cc8cbd20
Marvin Smith <marvin.smith@terminus-geo.com>
2025-01-03 14:16:40 -0700

    chore: Bumping versions.

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ from conan.tools.files import copy
 class ConanProject(ConanFile):
 
     name = "terminus_image"
-    version = "0.0.5"
+    version = "0.0.6"
 
     license = "Terminus Proprietary"
     author  = "Marvin Smith <marvin_smith1@me.com>"
@@ -32,18 +32,18 @@ class ConanProject(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     def build_requirements(self):
-        self.test_requires("gtest/1.14.0")
-        self.tool_requires("terminus_cmake/1.0.3")
+        self.test_requires("gtest/1.15.0")
+        self.tool_requires("terminus_cmake/1.0.5")
 
     def requirements(self):
-        self.requires("boost/1.85.0")
+        self.requires("boost/1.86.0")
         #self.requires("gdal/3.4.3")
         self.requires("nlohmann_json/3.11.3")
         self.requires("tomlplusplus/3.4.0")
-        self.requires("terminus_core/0.0.6")
-        self.requires("terminus_log/0.0.5")
-        self.requires("terminus_math/0.0.13")
-        self.requires("terminus_outcome/0.0.4")
+        self.requires("terminus_core/0.0.9")
+        self.requires("terminus_log/0.0.7")
+        self.requires("terminus_math/0.0.14")
+        self.requires("terminus_outcome/0.0.6")
 
     def _configure_cmake(self):
         cmake = CMake(self)
